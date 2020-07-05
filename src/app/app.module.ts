211d049c8135af2cdfe4f762f64dd40c7fa895d4
Duc <vanduc2514@gmail.com>
2020-07-05 18:35:44 +0700

    Update Rating Bar with directive Update pipe-demo with date pipe and uppercase pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CountdownTimerComponent } from './countdown-timer/countdown-timer.component';
-import { MessageTimerComponent } from './message-timer/message-timer.component';
-import {FormsModule} from "@angular/forms";
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {CountdownTimerComponent} from './countdown-timer/countdown-timer.component';
+import {MessageTimerComponent} from './message-timer/message-timer.component';
+import {FormsModule} from '@angular/forms';
+import {RatingBarComponent} from './rating-bar/rating-bar.component';
+import {PipeDemoComponent} from './pipe-demo/pipe-demo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CountdownTimerComponent,
     MessageTimerComponent,
+    RatingBarComponent,
+    PipeDemoComponent,
   ],
   imports: [
     BrowserModule,
@@ -19,6 +23,7 @@ import {FormsModule} from "@angular/forms";
     FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, RatingBarComponent, PipeDemoComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
